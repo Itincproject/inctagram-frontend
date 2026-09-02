e8f97db327bfcd5322a7843a60b2ab637652641f
Alexandr <sasha.zhulega.02@mail.ru>
2026-09-02 22:21:54 +0300

    CPZA-25: добавил заглушки  страниц

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Sidebar } from "../widgets/sidebars/ui/UserSidebar";
-import { Profile } from "../widgets/profile/ui/Profile";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,9 +20,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
