e8638fd6ea3cce1530100fbf2815d4c730d312b0
InnaShamulina <innaaf@yandex.ru>
2026-09-25 18:03:41 +0300

    CPZA-31 fix: Recaptcha component

--- shared/ui/Recaptcha/Recaptcha.tsx
+++ shared/ui/Recaptcha/Recaptcha.tsx
@@ -6,58 +6,65 @@ export type StatusType = 'idle' | 'loading' | 'success' | 'error' | 'expired'
 export type Props = {
     onClick: () => void
     status: StatusType
+    className: string
 }
 
-export const Recaptcha = ({ onClick, status }: Props) => {
+export const Recaptcha = ({ onClick, status, className }: Props) => {
     const errorMessage = {
         error: 'Please verify that you are not a robot',
         expired: 'Verifiction expired. Check the checkbox again.',
     }
     const isError = status === 'error'
     const isExpired = status === 'expired'
-    return (
-        <div className={`${styles.invisible_wrapper} ${isError ? styles.error_wrapper : ''}`}>
-            <div className={styles.conteiner}>
-                {isExpired && (
-                    <span className={`${styles.errorMessage} ${styles.expired}`}>{errorMessage.expired}</span>
-                )}
-                <div className={styles.captcha_conteiner}>
-                    <input
-                        type="checkbox"
-                        onChange={onClick}
-                        className={styles.checkbox}
-                        id="captcha"
-                        disabled={status === 'loading' || status === 'success'}
-                    />
-                    <div className={styles.btn_container}>
-                        {status === 'loading' ? (
-                            <RecaptchaLoader />
-                        ) : status === 'success' ? (
-                            <Icon name="success_verify" />
-                        ) : (
-                            <span className={styles.square} />
-                        )}
-                    </div>
 
-                    <label htmlFor="captcha" className={styles.label}>
-                        I’m not a robot
-                    </label>
+    const body = (
+        <div className={styles.conteiner}>
+            {isExpired && <span className={`${styles.errorMessage} ${styles.expired}`}>{errorMessage.expired}</span>}
+            <div className={styles.captcha_conteiner}>
+                <input
+                    type="checkbox"
+                    onChange={onClick}
+                    className={styles.checkbox}
+                    id="captcha"
+                    disabled={status === 'loading' || status === 'success'}
+                />
+                <div className={styles.btn_container}>
+                    {status === 'loading' ? (
+                        <RecaptchaLoader />
+                    ) : status === 'success' ? (
+                        <Icon name="success_verify" />
+                    ) : (
+                        <span className={styles.square} />
+                    )}
                 </div>
-                <div className={styles.logo_container}>
-                    <Icon name="recaptcha" size={31} className={styles.logo} />
-                    <span>reCAPTCHA</span>
-                    <div className={styles.link_container}>
-                        <a className={styles.link} href="https://policies.google.com/privacy">
-                            Privacy
-                        </a>
-                        <span className={styles.separator}>-</span>
-                        <a className={styles.link} href="https://policies.google.com/terms">
-                            Terms
-                        </a>
-                    </div>
+
+                <label htmlFor="captcha" className={styles.label}>
+                    I’m not a robot
+                </label>
+            </div>
+            <div className={styles.logo_container}>
+                <Icon name="recaptcha" size={31} className={styles.logo} />
+                <span>reCAPTCHA</span>
+                <div className={styles.link_container}>
+                    <a className={styles.link} href="https://policies.google.com/privacy">
+                        Privacy
+                    </a>
+                    <span className={styles.separator}>-</span>
+                    <a className={styles.link} href="https://policies.google.com/terms">
+                        Terms
+                    </a>
                 </div>
             </div>
-            {isError && <span className={styles.errorMessage}>{errorMessage.error}</span>}
         </div>
     )
+
+    if (isError)
+        return (
+            <div className={`${styles.error_wrapper} ${className}`}>
+                {body}
+
+                {isError && <span className={styles.errorMessage}>{errorMessage.error}</span>}
+            </div>
+        )
+    return <div className={className}>{body}</div>
 }
